23c6ee8db5f6d8969579bbaccd66f8b2c920dc17
Diversity <karim.1030@live.fr>
2024-04-21 00:34:19 +0200

    The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Diversity
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Diversity
+
+#pragma once
+
+#include "CoreMinimal.h"
+
